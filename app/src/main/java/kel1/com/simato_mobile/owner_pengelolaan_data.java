4f43c659cc6e0e9cd516a3ceec431313685e9918
sintaxd <jasintaxd@yahoo.com>
2019-04-06 16:18:03 +0700

    semangat sinta

---
+++ app/src/main/java/kel1/com/simato_mobile/owner_pengelolaan_data.java
@@ -0,0 +1,15 @@
+package kel1.com.simato_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class owner_pengelolaan_data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_pengelolaan_data);
+        getSupportActionBar().setTitle("Menu Utama");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
