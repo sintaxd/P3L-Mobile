c6d65998cf26c2acb5bde6439eb99c5c10132b8d
sintaxd <jasintaxd@yahoo.com>
2019-04-10 06:23:51 +0700

    ...

--- app/src/main/java/kel1/com/simato_mobile/ApiClient.java
+++ app/src/main/java/kel1/com/simato_mobile/ApiClient.java
@@ -3,17 +3,18 @@ package kel1.com.simato_mobile;
 import com.google.gson.JsonObject;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface ApiClient {
+    //  API CLIENT SUPPLIER //
 
-    //API CLIENT SUPPLIER
-    @GET("api/supplier")
-    Call<SupplierDAO> show();
-
+    //  CREATE
     @POST("api/supplier")
     @FormUrlEncoded
     Call<SupplierDAO> create(
@@ -23,5 +24,23 @@ public interface ApiClient {
             @Field("nama_sales")String nama_sales,
             @Field("noTelp_sales")String noTelp_sales);
 
+    //  READ
+    @GET("api/supplier")
+    Call<SupplierDAO> show();
+
+    //  UPDATE
+    @PUT("api/supplier/{id}")
+    @FormUrlEncoded
+    Call<SupplierDAO> update(
+            @Field("nama_supplier")String nama_supplier,
+            @Field("noTelp_supplier")String noTelp_supplier,
+            @Field("alamat_supplier")String alamat_supplier,
+            @Field("nama_sales")String nama_sales,
+            @Field("noTelp_sales")String noTelp_sales,
+            @Path("id_supplier") Integer id_supplier);
+
+    //  DELETE
+    @DELETE("api/supplier/{id}")
+    Call<SupplierDAO>delete(@Path("id_supplier") Integer id_supplier);
 
 }
--- app/src/main/java/kel1/com/simato_mobile/tambah_data_sparepart.java
+++ app/src/main/java/kel1/com/simato_mobile/tambah_data_sparepart.java
@@ -19,12 +19,17 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class tambah_data_sparepart extends AppCompatActivity {
 
     private Button btnBatal,btnSimpan;
-    private TextInputLayout nama_sparepart, merk_sparepart, kode_sparepart, tipe_sparepart;
+    private TextInputEditText nama_sparepart, merk_sparepart, kode_sparepart, tipe_sparepart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_data_sparepart);
-        setAtribut();
+        nama_sparepart = findViewById(R.id.text_input_namaSparepart);
+        merk_sparepart = findViewById(R.id.text_input_merkSparepart);
+        tipe_sparepart = findViewById(R.id.text_input_tipeSparepart);
+        kode_sparepart = findViewById(R.id.text_input_kode_sparepart);
+        btnSimpan = findViewById(R.id.button_Simpan);
+        btnBatal = (Button)findViewById(R.id.button_Batal);
         btnSimpan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,31 +39,23 @@ public class tambah_data_sparepart extends AppCompatActivity {
         btnBatal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                nama_sparepart.getEditText().getText().clear();
-                merk_sparepart.getEditText().getText().clear();
-                tipe_sparepart.getEditText().getText().clear();
-                kode_sparepart.getEditText().getText().clear();
+                nama_sparepart.getText().clear();
+                merk_sparepart.getText().clear();
+                tipe_sparepart.getText().clear();
+                kode_sparepart.getText().clear();
             }
         });
 
 
     }
-    private void setAtribut() {
-        nama_sparepart = findViewById(R.id.text_input_namaSparepart);
-        merk_sparepart = findViewById(R.id.text_input_merkSparepart);
-        tipe_sparepart = findViewById(R.id.text_input_tipeSparepart);
-        kode_sparepart = findViewById(R.id.text_input_kode_sparepart);
-        btnSimpan = findViewById(R.id.button_Simpan);
-        btnBatal = (Button)findViewById(R.id.button_Batal);
-    }
     private void startIntent() {
         Intent intent = new Intent(getApplicationContext(), tampil_data_sparepart.class);
         startActivity(intent);
     }private void onClickRegister() {
-        if (nama_sparepart.getEditText().getText().toString().isEmpty() ||
-            merk_sparepart.getEditText().getText().toString().isEmpty() ||
-            tipe_sparepart.getEditText().getText().toString().isEmpty() ||
-            kode_sparepart.getEditText().getText().toString().isEmpty())
+        if (nama_sparepart.getText().toString().isEmpty() ||
+            merk_sparepart.getText().toString().isEmpty() ||
+            tipe_sparepart.getText().toString().isEmpty() ||
+            kode_sparepart.getText().toString().isEmpty())
         {
             Toast.makeText(this, "Field can't be empty", Toast.LENGTH_SHORT).show();
         } else {
@@ -68,10 +65,10 @@ public class tambah_data_sparepart extends AppCompatActivity {
                     .addConverterFactory(GsonConverterFactory.create());
             Retrofit retrofit = builder.build();
             ApiClientSparepart apiClient = retrofit.create(ApiClientSparepart.class);
-            Call<SparepartDAO> sparepartDAOCall = apiClient.create(nama_sparepart.getEditText().getText().toString(),
-                    merk_sparepart.getEditText().getText().toString(),
-                    tipe_sparepart.getEditText().getText().toString(),
-                    kode_sparepart.getEditText().getText().toString());
+            Call<SparepartDAO> sparepartDAOCall = apiClient.create(nama_sparepart.getText().toString(),
+                    merk_sparepart.getText().toString(),
+                    tipe_sparepart.getText().toString(),
+                    kode_sparepart.getText().toString());
             sparepartDAOCall.enqueue(new Callback<SparepartDAO>() {
                 @Override
                 public void onResponse(Call<SparepartDAO> call, Response<SparepartDAO> response) {
